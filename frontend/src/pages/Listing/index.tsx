64d36d6ab92f792aab75033b42cb62fbd6c5f4bd
Itor-Carlos <itor51carlos@gmail.com>
2022-01-15 23:41:01 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -31,9 +31,14 @@ function Listing(){
             })
     },[pageNumber])
 
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber);
+    }
+
+
     return(
         <>
-            <Pagination/>
+            <Pagination page = {page} onChange={handlePageChange}/>
 
             <div className="container">
                 <div className="row">
