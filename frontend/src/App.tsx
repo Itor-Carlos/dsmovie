88907a97ca36007ddc23b0b87445e1b4fe4b1127
Itor-Carlos <itor51carlos@gmail.com>
2022-01-15 23:15:06 -0300

    useParams

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import {
 import Listing from 'pages/Listing';
 import Form from 'pages/Form';
 import Navbar from "components/Navbar";
+import FormCard from "components/FormCard";
 
 function App() {
   return (
